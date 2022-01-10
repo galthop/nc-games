5fcb57e0fab85a95632f0693502f50d026848764
galthop <hitenpatel365@gmail.com>
2022-01-10 15:39:28 +0000

    All component files created

---
+++ src/components/Nav.jsx
@@ -0,0 +1,5 @@
+const Nav = () => {
+  return <h1>Navigation bar</h1>;
+};
+
+export default Nav;
---
+++ src/components/ReviewsPage.jsx
@@ -0,0 +1,5 @@
+const ReviewsPage = () => {
+  return <h1>This is the homepage</h1>;
+};
+
+export default ReviewsPage;
